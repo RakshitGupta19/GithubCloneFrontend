5a3491b0510640a785b64b9005f4330c63ab0469
Rakshit <rakshit.gupta229@gmail.com>
2025-07-24 19:53:14 +0530

    some files updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Expose to external network (Render)
+    port: process.env.PORT || 5173, // Use Render-assigned port if available
+  },
 })
